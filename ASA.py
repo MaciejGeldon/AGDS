f3aee15ae21f9bdca9f24138fe3edb18670e0a94
maciejg <einstein1905meister@gmail.com>
2020-11-18 21:41:42 +0100

    Adding not finished delete and tests to existing steps

--- ASA.py
+++ ASA.py
@@ -53,6 +53,17 @@ class ASABaseElem:
         self.successor = elem
         elem.predecessor = self
 
+    def delete(self, element):
+        if element.successor and element.predecessor:
+            element.predecessor.successor = element.successor
+            element.successor.predecessor = element.predecessor
+
+        elif element.successor:
+            element.successor.predecessor = element.predecessor
+
+        else:
+            element.predecessor.successor = element.successor
+
 
 class ASABaseContainer:
     def __init__(self):
@@ -77,6 +88,14 @@ class ASABaseContainer:
             new_elem.link_after(element, self)
 
         return new_elem
+
+    def delete(self, element):
+        if element == self.max:
+            self.max = element.predecessor
+        elif element == self.min:
+            self.min = element.successor
+
+        element.delete(element)
 
     def __iter__(self):
         current = self.min
@@ -109,7 +128,12 @@ class ASATreeNode:
 
         if not node.leaf:
             node_left.children = node.children[:node.t + 1]
+            for ch_l in node_left.children:
+                ch_l.parent = node_left
+
             node_right.children = node.children[node.t + 1:]
+            for ch_r in node_right.children:
+                ch_r.parent = node_right
 
         return promoted_element, node_left, node_right
 
@@ -216,9 +240,33 @@ class ASA:
             if l.successor is r:
                 return (l.key + r.key) / 2
             else:
+                if l.successor == r.predecessor:
+                    return l.successor
+
                 l = l.successor
                 r = r.predecessor
                 return self._median((l.count, r.count), l, r)
+
+    def search(self, key):
+        if self.root is None:
+            return False, self.root
+
+        return self._search(key, self.root)
+
+    def _search(self, key, node):
+        if node.leaf:
+            for k in node.keys:
+                if k == key:
+                    return k, node
+            return False, None
+
+        for i, k in enumerate(node.keys):
+            if k == key:
+                return k, node
+            elif key < k:
+                return self._search(key, node.children[i])
+
+        return self._search(key, node.children[-1])
 
     def insert(self, key):
         if self.root is None:
@@ -227,6 +275,15 @@ class ASA:
             return
 
         self._insert(key, self.root)
+
+    def _insert(self, key, node):
+        if node.leaf:
+            node.add_new(key, self.asa_container)
+            if node.overflow:
+                self._split_and_propagate(node)
+        else:
+            ch_index = self._get_next_node_index(key, node)
+            self._insert(key, node.children[ch_index])
 
     def _create_new_root(self, median_key, left_child, right_child):
         new_root = ASATreeNode()
@@ -275,14 +332,108 @@ class ASA:
             if parent.overflow:
                 self._split_and_propagate(parent)
 
-    def _insert(self, key, node):
-        if node.leaf:
-            node.add_new(key, self.asa_container)
-            if node.overflow:
-                self._split_and_propagate(node)
+    def delete(self, key):
+        empty_leaf = False
+
+        if self.root is None:
+            return False
+
+        key, node = self.search(key)
+        if key is False:
+            return False
+
+        elif key.count > 1:
+            key.count -= 1
+            return key
+
+        elif node.leaf:
+            if len(node.keys) > 1:
+                node.keys.remove(key)
+                self.asa_container.delete(key)
+                return key
+            else:
+                node.keys.remove(key)
+                empty_leaf = node
         else:
-            ch_index = self._get_next_node_index(key, node)
-            self._insert(key, node.children[ch_index])
+            empty_leaf = self._replace_with_leaf_candidate(key, node)
+            if not empty_leaf:
+                return False
+
+        if empty_leaf:
+            sibling_shift_resolution = self._try_siblings(empty_leaf)
+            if isinstance(sibling_shift_resolution, ASATreeNode):
+                return sibling_shift_resolution
+
+        # here I need to implement steps 6,7,8,9 from delete
+
+    # this needs better algorithm 
+    def _replace_with_leaf_candidate(self, elem, elem_node):
+        predecessor, p_node = self.search(elem.predecessor)
+        successor, s_node = self.search(elem.successor)
+
+        if len(p_node.keys) > 1:
+            predecessor.successor = elem.successor
+            elem.successor.predecessor = predecessor
+            elem_node.keys.insert(elem_node.keys.index(elem), predecessor)
+            elem_node.keys.remove(elem)
+            p_node.keys.remove(predecessor)
+            return False
+
+        if len(s_node.keys) > 1:
+            successor.predecessor = elem.predecessor
+            elem.predecessor.successor = successor
+            elem_node.keys.insert(elem_node.keys.index(elem), successor)
+            s_node.keys.remove(successor)
+            elem_node.keys.remove(elem)
+            return False
+
+        p_node_par = p_node.parent
+        s_node_par = s_node.parent
+
+        if p_node_par is not elem_node:
+            if p_node_par.keys >= s_node_par.keys:
+                predecessor.successor = elem.successor
+                elem.successor.predecessor = predecessor
+                elem_node.insert(elem_node.keys.index(elem), predecessor)
+                elem_node.keys.remove(elem)
+                p_node.remove(predecessor)
+                return p_node
+            else:
+                successor.predecessor = elem.predecessor
+                elem.predecessor.successor = successor
+                elem_node.insert(elem_node.keys.index(elem), predecessor)
+                s_node.keys.remove(successor)
+                elem_node.keys.remove(elem)
+                return s_node
+
+        predecessor.successor = elem.successor
+        elem.successor.predecessor = predecessor
+        elem_node.insert(elem_node.keys.index(elem), predecessor)
+        elem_node.keys.remove(elem)
+        p_node.remove(predecessor)
+        return p_node
+
+    def _try_siblings(self, empty_leaf):
+        parent = empty_leaf.parent
+
+        c_ind = None
+        candidate = None
+        empty_index = None
+
+        for i, ch in enumerate(parent.children):
+            if len(ch.keys) > 1:
+                c_ind = i
+                candidate = ch
+            elif empty_leaf == ch:
+                empty_index = i
+
+        if abs(c_ind - empty_index) == 1:
+            ch_draw_ind = int(bool(c_ind - empty_index < 0))
+            parent.keys.insert(c_ind, candidate.keys.pop(ch_draw_ind))
+            empty_leaf.keys.append(parent.keys.pop(empty_index))
+            return empty_leaf
+
+        return False
 
 
 if __name__ == '__main__':
--- test_asa.py
+++ test_asa.py
@@ -92,6 +92,20 @@ def test_should_compose_3_level_tree_and_have_proper_structure():
     assert r_tree.children[1].leaf
 
 
+def test_asa_should_have_proper_parent_element_structure():
+    asa = ASA()
+    for key in [2, 3, 5, 7, 8, 9, 10, 11]:
+        asa.insert(key)
+
+    elem, node = asa.search(11)
+
+    assert node.parent.keys == [9]
+    grandparent = node.parent.parent
+
+    assert grandparent == asa.root
+    assert asa.root.keys == [7]
+
+
 def test_asa_should_calculate_proper_avr_and_sum():
     asa = ASA()
 
@@ -153,7 +167,8 @@ def test_should_calculate_median_1():
     "elements", [
         [1, 1, 2], [1, 2], [5],
         [1, 2, 2, 3, 3, 4], [5, 5, 5],
-        [1, 1, 1, 2, 2, 3, 4, 5, 6]
+        [1, 1, 1, 2, 2, 3, 4, 5, 6],
+        [2, 9, 1, 4, 5, 3, 6]
     ]
 )
 def test_should_check_median_for_given_elements(elements):
@@ -163,3 +178,241 @@ def test_should_check_median_for_given_elements(elements):
         asa.insert(e)
 
     assert asa.median == median(elements)
+
+
+def test_asa_search_empty_asa_should_return_node():
+    asa = ASA()
+    found, _ = asa.search(10)
+    assert found is False
+
+
+@pytest.mark.parametrize(
+    "elements", [
+        [1, 1, 2], [1, 2], [5],
+        [1, 2, 2, 3, 3, 4], [5, 5, 5],
+        [1, 1, 1, 2, 2, 3, 4, 5, 6],
+        [2, 9, 1, 4, 5, 3, 6]
+    ]
+)
+def test_should_return_false_for_given_asa_trees(elements):
+    asa = ASA()
+
+    for e in elements:
+        asa.insert(e)
+    found, _ = asa.search(10)
+    assert found is False
+
+
+@pytest.mark.parametrize(
+    "elements,key", [
+        ([1, 1, 2], 1),
+        ([1, 2, 2, 3, 3, 4], 4),
+        ([1, 1, 1, 2, 2, 3, 4, 5, 6], 5),
+        ([2, 9, 1, 4, 5, 3, 6], 9),
+        ([2, 9, 1, 4, 5, 3, 6, 10], 6),
+        ([1, 2, 4, 5, 9], 4)
+    ]
+)
+def test_should_return_element_for_given_asa_trees_and_search_keys(elements, key):
+    asa = ASA()
+
+    for e in elements:
+        asa.insert(e)
+
+    found, _ = asa.search(key)
+    assert found
+    assert found.key == key
+
+
+def test_delete_for_initial_case():
+    asa = ASA()
+
+    assert asa.delete(1) is False
+
+
+@pytest.mark.parametrize(
+    'elements,delete_key,count',
+    [
+        ([1, 1, 2], 1, 1),
+        ([1, 2, 2, 3, 3, 4, 4], 4, 1),
+        ([1, 1, 1, 2, 2, 3, 4, 5, 5, 6], 5, 1),
+        ([2, 9, 9, 9, 1, 4, 5, 3, 6], 9, 2),
+        ([2, 9, 1, 4, 5, 3, 6, 6, 6, 6, 10], 6, 3),
+    ]
+)
+def test_delete_should_work_for_existing_duplicated_key(elements, delete_key, count):
+    asa = ASA()
+    for key in elements:
+        asa.insert(key)
+
+    deleted = asa.delete(delete_key)
+    assert isinstance(deleted, ASABaseElem)
+    assert deleted.count == count
+
+
+def test_delete_should_return_false_if_key_for_delete_not_found():
+    asa = ASA()
+    for i in [1, 2, 2, 3, 3, 4, 4]:
+        asa.insert(i)
+
+    assert asa.delete(10) is False
+
+
+def test_delete_should_remove_key_from_node_with_two_keys_without_rebalancing():
+    asa = ASA()
+    for i in [2, 3, 5, 9]:
+        asa.insert(i)
+
+    key = asa.delete(5)
+    assert isinstance(key, ASABaseElem)
+
+    found, node = asa.search(5)
+
+    assert found is False and node is None
+
+    assert [e.key for e in asa.asa_container] == [2, 3, 9]
+
+    found, node = asa.search(9)
+    assert found
+
+    assert node.parent.keys == [3]
+    assert node.leaf is True
+
+
+def test_delete_should_remove_key_from_node_with_two_keys_without_rebalancing_v2():
+    asa = ASA()
+    for i in [2, 3, 5, 7, 8, 9, 10, 11]:
+        asa.insert(i)
+
+    key = asa.delete(10)
+    assert isinstance(key, ASABaseElem)
+
+    found, node = asa.search(10)
+
+    assert found is False and node is None
+
+    assert [e.key for e in asa.asa_container] == [2, 3, 5, 7, 8, 9, 11]
+
+    found, node = asa.search(11)
+    assert found
+
+    assert node.parent.keys == [9]
+    assert node.leaf is True
+
+
+@pytest.mark.parametrize(
+    'elem,node_elem',
+    [(6.5, 6), (7.5, 8)]
+)
+def test_delete_should_replace_deleted_node_key_with_leaf_key_when_proper_leaf_key_found(elem, node_elem):
+    asa = ASA()
+    for i in range(13):
+        asa.insert(i)
+
+    # this will be predecessor or successor of 7
+    asa.insert(elem)
+
+    _, node = asa.search(node_elem)
+
+    assert len(node.keys) == 2
+
+    assert asa.delete(7) is False
+    assert elem in asa.root.keys
+
+    elem, node = asa.search(node_elem)
+    assert len(node.keys) == 1
+
+
+@pytest.mark.parametrize(
+    'elem,node_elem',
+    [(0.1, 0), (1.5, 2)]
+)
+def test_delete_should_handle_one_level_tree_case(elem, node_elem):
+    asa = ASA()
+    for i in range(3):
+        asa.insert(i)
+
+    asa.insert(elem)
+
+    _, node = asa.search(node_elem)
+
+    assert len(node.keys) == 2
+
+    assert asa.delete(1) is False
+    assert elem in asa.root.keys
+
+    elem, node = asa.search(node_elem)
+    assert len(node.keys) == 1
+
+
+@pytest.mark.parametrize(
+    'add, delete, empty_value',
+    [
+        (0.1, 2, 1),
+        (1.1, 0, 1),
+        (1.1, 4, 3),
+        (3.5, 2, 3)
+    ]
+)
+def test_delete_should_handle_filing_empty_leaf_for_one_level_tree(add, delete, empty_value):
+    asa = ASA()
+    for i in range(5):
+        asa.insert(i)
+
+    asa.insert(add)
+
+    modified_node = asa.delete(delete)
+
+    assert empty_value == modified_node.keys[0].key
+
+    # check if structure is preserved
+    parent = modified_node.parent
+    root = asa.root
+
+    assert parent == root
+    assert len(parent.keys) == 2
+
+    p_keys = parent.keys
+    assert p_keys[0] < p_keys[1]
+
+    ch_1, ch_2, ch_3 = parent.children
+    assert ch_1.keys[0].key < p_keys[0].key < ch_2.keys[0].key
+    assert ch_2.keys[0].key < p_keys[1].key < ch_3.keys[0].key
+
+    assert ch_1.keys[0].key < ch_2.keys[0].key < ch_3.keys[0].key
+    assert len(ch_1.keys) == len(ch_2.keys) == len(ch_3.keys) == 1
+
+
+@pytest.mark.parametrize(
+    'add, delete, empty_value',
+    [
+        (0.1, 2, 1),
+        (1.1, 0, 1),
+    ]
+)
+def test_delete_should_handle_filing_empty_leaf_for_one_level_tree_and_two_children(add, delete, empty_value):
+    asa = ASA()
+    for i in range(3):
+        asa.insert(i)
+
+    asa.insert(add)
+
+    modified_node = asa.delete(delete)
+
+    assert empty_value == modified_node.keys[0].key
+
+    # check if structure is preserved
+    parent = modified_node.parent
+    root = asa.root
+
+    assert parent == root
+    assert len(parent.keys) == 1
+
+    p_keys = parent.keys
+    ch_1, ch_2 = parent.children
+
+    assert ch_1.keys[0].key < p_keys[0].key < ch_2.keys[0].key
+
+    assert ch_1.keys[0].key < ch_2.keys[0].key
+    assert len(ch_1.keys) == len(ch_2.keys) == 1
+
